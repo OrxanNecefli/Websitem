5bacfc5caa9a7088980c5111a18cc711dc4417ef
OrxanNecefli <orxannecefli944@gmail.com>
2019-09-28 18:13:14 +0400

    son deyisiklik

--- git.js
+++ git.js
@@ -4,3 +4,12 @@ function func() {
     metn2.innerHTML = "javascript element";
     $("body").append(metn1, metn2)
 }
+$(document).ready(function() {
+    $("#btn").click(function() {
+        var bir = $("<b></b>").text("bold yarat");
+        var iki = $("<input type='text'/>").val("input")
+        var uc = document.createElement("strong");
+        uc.innerHTML = "strong element"
+        $('p').append(bir, iki, uc)
+    })
+})
