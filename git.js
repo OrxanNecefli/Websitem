20a6d6324847597bbb39af656b907ef201d4001c
OrxanNecefli <orxannecefli944@gmail.com>
2019-09-29 01:00:57 +0400

    bugunluk ders bitdi

--- git.js
+++ git.js
@@ -1,10 +1,14 @@
-$(document).ready(function() {
-    $("#btn").click(function() {
-        var txt = "";
-        txt += $(document).width() + " px "
-        txt += $(document).height() + " px <br/>"
-        txt += $(window).height() + " px "
-        txt += $(window).width() + " px "
-        $("h4").html(txt)
-    });
-})
+// var ard = ["Nəbi", "Orxan", "Arif", "Bahar", "Rəsul", "Ümid", "ördək"];
+
+Array.prototype.shuffle = function() {
+    var element = this.length;
+    while (--element > 0) {
+        var say = Math.floor(Math.random()) + (element + 1);
+        var herf = this[say]
+    }
+}
+
+var ard = ['A', 'B', 'C', 'D', 'E'];
+document.write(ard + "<br/>");
+var son = ard.shuffle();
+// document.write(son);
