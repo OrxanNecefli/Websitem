a684a79a2275640c1148f0f4cd3c6367f26fe4b1
OrxanNecefli <orxannecefli944@gmail.com>
2019-09-28 19:57:37 +0400

    bitdi

--- git.js
+++ git.js
@@ -1,15 +1,10 @@
-function func() {
-    var metn1 = $("<p></p>").text("jquery yaradilmish \"P\" elementi");
-    var metn2 = document.createElement("P");
-    metn2.innerHTML = "javascript element";
-    $("body").append(metn1, metn2)
-}
 $(document).ready(function() {
     $("#btn").click(function() {
-        var bir = $("<b></b>").text("bold yarat");
-        var iki = $("<input type='text'/>").val("input")
-        var uc = document.createElement("strong");
-        uc.innerHTML = "strong element"
-        $('p').append(bir, iki, uc)
-    })
+        var txt = "";
+        txt += $(document).width() + " px "
+        txt += $(document).height() + " px <br/>"
+        txt += $(window).height() + " px "
+        txt += $(window).width() + " px "
+        $("h4").html(txt)
+    });
 })
