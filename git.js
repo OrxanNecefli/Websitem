4358198ca0eeef56f49f7365a3ec53cada6fedfd
OrxanNecefli <orxannecefli944@gmail.com>
2019-09-29 11:53:54 +0400

    shuffle metodu rastgele herfler ve ya reqemler kodu

--- git.js
+++ git.js
@@ -1,14 +1,14 @@
-// var ard = ["Nəbi", "Orxan", "Arif", "Bahar", "Rəsul", "Ümid", "ördək"];
-
 Array.prototype.shuffle = function() {
-    var element = this.length;
-    while (--element > 0) {
-        var say = Math.floor(Math.random()) + (element + 1);
-        var herf = this[say]
+    var elmnsay = this.length;
+    while (--elmnsay > 0) {
+        var rstsay = Math.floor(Math.random() * 6);
+        var bir = this[rstsay];
+        this[rstsay] = this[elmnsay]
+        this[elmnsay] = bir;
+        return this;
     }
 }
-
-var ard = ['A', 'B', 'C', 'D', 'E'];
-document.write(ard + "<br/>");
-var son = ard.shuffle();
-// document.write(son);
+var herf = ["A", 'B', 'C', "D", "E"];
+document.write(herf + "<br/>")
+var shuf = herf.shuffle()
+document.write(shuf)
