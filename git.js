f7fd4ed2b969615c8e98243c40f3b14ca9af1641
OrxanNecefli <orxannecefli944@gmail.com>
2019-09-30 13:09:59 +0400

    Regexpe basladim

--- git.js
+++ git.js
@@ -1,1 +1,5 @@
-document.write(Math.exp(4) + "<br/>" + Math.log(0))
+var txt = "Orxan \n necesen Orxan, \n orxaN";
+var ifade = new RegExp('^orxan', 'mi');
+alert(txt.match(ifade));
+var kontrol = ifade.ignoreCase;
+document.write(mch + "<br/>" + kontrol)
