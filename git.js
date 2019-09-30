5ca78d9441970ae1bdc04bff45fbce99cfe18a2b
OrxanNecefli <orxannecefli944@gmail.com>
2019-09-30 11:18:03 +0400

    tarix : 9/30/2019           saat: 11:17

--- git.js
+++ git.js
@@ -1,14 +1,1 @@
-Array.prototype.shuffle = function() {
-    var elmnsay = this.length;
-    while (--elmnsay > 0) {
-        var rstsay = Math.floor(Math.random() * 6);
-        var bir = this[rstsay];
-        this[rstsay] = this[elmnsay]
-        this[elmnsay] = bir;
-        return this;
-    }
-}
-var herf = ["A", 'B', 'C', "D", "E"];
-document.write(herf + "<br/>")
-var shuf = herf.shuffle()
-document.write(shuf)
+document.write(Math.exp(4) + "<br/>" + Math.log(0))
